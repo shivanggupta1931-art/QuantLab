fdd3c7ae74eb64203ace7840e02e9f415486fcbc
Shivang Gupta <shivanggupta1931@gmail.com>
2026-07-20 22:05:41 +0530

    new features added

--- js/replay/app.js
+++ js/replay/app.js
@@ -1,16 +1,45 @@
 
 
 
-document.addEventListener("DOMContentLoaded",async()=>{
-        console.log("Before initChart");
-        initChart();
-        console.log("after chartinit")
-        const data=await loadMarketData("data/HitBTC_BTCUSD_d.csv");
-        startReplay(data);
-        document.getElementById("nextCandleBtn").addEventListener("click",nextCandle);
-        document.querySelector(".buy-btn").addEventListener("click",buy);
-        document.querySelector(".sell-btn").addEventListener("click",sell);
+// document.addEventListener("DOMContentLoaded",async()=>{
 
-document.querySelector(".close-btn").addEventListener("click",closeTrade)
+
+//         console.log("Before initChart");
+//         initChart();
+//         console.log("after chartinit")
+//         const data=await loadMarketData("data/HitBTC_BTCUSD_d.csv");
+//         startReplay(data);
+//         document.getElementById("nextCandleBtn").addEventListener("click",nextCandle);
+//         document.querySelector(".buy-btn").addEventListener("click",buy);
+//         document.querySelector(".sell-btn").addEventListener("click",sell);
+
+// document.querySelector(".close-btn").addEventListener("click",closeTrade)
+// });
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    initChart();
+
+    const data = await loadMarketData("data/HitBTC_BTCUSD_d.csv");
+    startReplay(data);
+
+    document.getElementById("nextCandleBtn").addEventListener("click", nextCandle);
+    document.querySelector(".buy-btn").addEventListener("click", buy);
+    document.querySelector(".sell-btn").addEventListener("click", sell);
+    document.querySelector(".close-btn").addEventListener("click", closeTrade);
+
+    // Keyboard shortcut
+    document.addEventListener("keydown", function (event) {
+        if (event.repeat) return;
+
+        const tag = event.target.tagName;
+
+        if (tag === "INPUT" || tag === "TEXTAREA" || tag === "SELECT") {
+            return;
+        }
+
+        if (event.key === "ArrowRight") {
+            nextCandle();
+        }
+    });
 });
-
