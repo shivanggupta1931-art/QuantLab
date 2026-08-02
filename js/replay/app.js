96eddbdfa6e9a2293cfb303f1e8bf4c5bbea9f6a
Shivang Gupta <shivanggupta1931@gmail.com>
2026-08-02 23:38:24 +0530

    feat:added Ethreum

--- js/replay/app.js
+++ js/replay/app.js
@@ -3,8 +3,35 @@
 document.addEventListener("DOMContentLoaded", async () => {
     initChart();
 
-    const data = await loadMarketData("data/HitBTC_BTCUSD_d.csv");
-    startReplay(data);
+    // const data = await loadMarketData("data/btc/HitBTC_BTCUSD_d.csv");
+    // let currentMarket = markets.btc;
+
+    // replay.allData = await loadMarketData(currentMarket.file);
+    // startReplay(data);
+
+//     let currentMarket = markets.btc;
+
+// const data = await loadMarketData(currentMarket.file);
+
+// replay.allData = data;
+
+// startReplay(data);
+
+
+await loadSelectedMarket("btc");
+
+const marketSelect = document.getElementById("marketSelect");
+
+marketSelect.addEventListener("change", async function () {
+
+    await loadSelectedMarket(this.value);
+
+});
+
+
+
+
+
 
     document.getElementById("nextCandleBtn").addEventListener("click", nextCandle);
     document.querySelector(".buy-btn").addEventListener("click", buy);
@@ -26,3 +53,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     });
 });
+
+
+async function loadSelectedMarket(marketKey){
+
+    const market = markets[marketKey];
+
+    replay.allData = await loadMarketData(market.file);
+
+    startReplay(replay.allData);
+
+}
---
+++ js/replay/markets.js
@@ -0,0 +1,15 @@
+const markets = {
+    btc: {
+        id: "btc",
+        name: "Bitcoin",
+        symbol: "BTC/USD",
+        file: "data/btc/HitBTC_BTCUSD_d.csv"
+    },
+
+    eth: {
+        id: "eth",
+        name: "Ethereum",
+        symbol: "ETH/USD",
+        file: "data/btc/ETHUSD.csv"
+    }
+};
